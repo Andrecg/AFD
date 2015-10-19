1333ad4afe3bb2a9a9c216a5200321f8cc4c53ac
Andre Garcia <andrecadgarcia@gmail.com>
2015-10-19 13:09:39 -0200

    Added javadoc

--- src/AFD/Main.java
+++ src/AFD/Main.java
@@ -3,8 +3,15 @@ package AFD;
 import java.io.File;
 import java.util.LinkedList;
 
+/**
+ * Main class, execution starts here
+ * @author Andre
+ * @author Aurelio
+ * @author Cesar
+ */
 public class Main {
 	public static void main(String[] args) {
+		//Create graphic interface
 		Gui g = new Gui();
 	}
 }
